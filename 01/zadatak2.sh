d0b2f5e595ce5b7e3fa218cac41310d387e18dd8
Siniša Bolšec <sinisa.bolsec0@gmail.com>
2021-03-22 16:41:44 +0100

    gotov drugi zadatak

--- 01/zadatak2.sh
+++ 01/zadatak2.sh
@@ -10,4 +10,21 @@ grep -ivE "banana|jabuka|jagoda|dinja|lubenica" namirnice.txt
 
 echo ""
 echo "c:"
-grep -rnET "[A-Z]{3}[0-9]{6}" ~/projekti/
+grep -rnET "\b[A-Z]{3}[0-9]{6}\b" ~/projekti/
+
+echo ""
+echo "d:"
+find . -mtime -14 -mtime 7 -ls
+
+echo ""
+echo "e:"
+for i in {1..15} ; do echo $i ; done
+# for i in $(seq 1 15) ; do echo $i ; done
+
+echo ""
+echo "f:"
+kraj=15
+echo "sekvenca:"
+for i in {1..$kraj} ; do echo $i ; done
+echo "seq:"
+for i in $(seq 1 $kraj) ; do echo $i ; done
